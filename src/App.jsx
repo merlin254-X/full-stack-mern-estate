0d3daa067a2a02d627fa6cad621234bc9c549963
merlin254-X <merlinderick8@gmail.com>
2024-12-06 15:54:45 +0300

    pages and routes creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import About from './pages/About';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
 
 export default function App() {
   return (
-    <hi className='text-red-500'>App</hi>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
