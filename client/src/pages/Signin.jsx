0fb033dc561a6d1546281318f24ce88cf839ebb9
merlin254-X <merlinderick8@gmail.com>
2025-01-03 22:31:00 +0300

    add redux tool kit and redux persist

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector }  from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+ const { loading, error } = useSelector((state) => state.user);  
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   // Handler for input change
   const handleChange = (e) => {
@@ -19,7 +21,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -30,16 +32,13 @@ export default function SignIn() {
       const data = await res.json();
       console.log(data);
       if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
+        dispatch(signInFailure(data.message));        
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data));      
       navigate('/');
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message));      
     }
   };
 
@@ -69,7 +68,7 @@ export default function SignIn() {
           />
         </div>
         <button type="submit" disabled={loading} className="bg-blue-500 text-white p-2 rounded-md w-full">
-          {loading ? 'Loading...' : 'Sign In'}
+          {loading ? 'Loading...' : 'Sign In'}s
         </button>
       </form>
       <div className="flex gap-2 mt-5">
