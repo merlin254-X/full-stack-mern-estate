d65a7abc0ea01fc519a1d25e34c2f3438f05802f
merlin254-X <merlinderick8@gmail.com>
2025-01-17 23:00:05 +0300

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -32,3 +32,24 @@ export const deleteListing = async (req, res, next) => {
         next(error);
       }
 };
+
+export const updateListing = async (req, res, next) => {
+    try {
+        const listing = await Listing.findById(req.params.id);
+        if (!listing) {
+            return next(errorHandler(404, 'Listing not found!'));
+        }
+        if (req.user.id !== listing.userRef) {
+            return next(errorHandler(401, 'You can only update your own listings!'));
+        }
+
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        return res.status(200).json(updatedListing);
+    } catch (error) {
+        next(error);
+    }
+}; 
