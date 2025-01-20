d3b19261058b1bc3041f9c72c613ef37d2b85878
Danchronicles <dankipchumba669@gmail.com>
2025-01-20 09:53:02 +0300

    userroute

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { deleteUser, test, updateUser, getUserListings } from '../controllers/user.controller.js';
+import { deleteUser, test, updateUser, getUserListings, getUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
+router.get('/:id', verifyToken, getUser);
 
 export const router = express.Router();
 
@@ -10,5 +11,3 @@ router.delete('/delete/:id', verifyToken, deleteUser);
 router.get('/listings/:id', verifyToken, getUserListings);
 
 export default router;
-
-router.get('/:id', verifyToken, getUser);
