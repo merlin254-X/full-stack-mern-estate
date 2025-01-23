ee069b6c5eeb5c39dd065348ac1466053bd24798
merlin254-X <merlinderick8@gmail.com>
2025-01-23 15:33:16 +0300

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About RealDml Estate</h1>
+      <p className='mb-4 text-slate-700'>
+        RealDml Estate is a premier real estate agency dedicated to helping clients navigate the property market with ease. Whether you're looking to buy, sell, or rent, we specialize in connecting you with the perfect property in the most sought-after locations.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Our mission is to empower clients by offering tailored solutions, expert guidance, and a thorough understanding of the local real estate landscape. We strive to make every transaction seamless, ensuring that your real estate journey is both enjoyable and successful.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        With a team of seasoned professionals, RealDml Estate brings years of industry expertise and a commitment to excellence. We believe that finding or selling a property should be a fulfilling experience, and we work tirelessly to make that a reality for every client we serve.
+      </p>
+    </div>
+  );
 }
