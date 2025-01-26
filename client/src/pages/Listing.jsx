d7bf157415e57e3fc1e96463b278dddb4a411795
Umuhoza Lionel <muhozalionel@gmail.com>
2025-01-26 02:30:27 -0500

    Test for button link option

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -99,9 +99,9 @@ export default function Listing() {
               {listing.address}
             </p>
             <div className='flex gap-4'>
-              <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
+              <a href="https://www.mernestate.com/buy" className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
                 {listing.type === 'rent' ? 'For Rent' : 'For Sale'}
-              </p>
+              </a>
               {listing.offer && (
                 <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
                   ${+listing.regularPrice - +listing.discountPrice} OFF
