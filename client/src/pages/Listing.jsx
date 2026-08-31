cef9fd157981c1d5cdc2d981d2f7f63392aebe46
Umuhoza Lionel <muhozalionel@gmail.com>
2026-08-31 16:29:30 +0200

    Fix Contact import case for Vercel

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -13,7 +13,7 @@ import {
   FaParking,
   FaShare,
 } from 'react-icons/fa';
-import Contact from '../component/Contact';
+import Contact from '../component/contact';
 
 export default function Listing() {
   SwiperCore.use([Navigation]);
